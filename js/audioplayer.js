015881cf21201ac033ef04b60e8a55130bb760f9
Pat Shiu <shiu.patricia@gmail.com>
2016-02-07 04:12:21 -0500

    Post-presentation clean ups. Page flip speed, updated about text. button styling.

--- js/audioplayer.js
+++ js/audioplayer.js
@@ -1,9 +1,20 @@
-interval = 34000;
+interval = 2700000; //2,700 seconds per 2 pages assuming 4.5seconds per reading
 page = 0;
 
 function playBook(){
 	document.getElementById('audio').play();
+	flipPageIntro();
+}
+
+function flipPageIntro(){
 	flipPage();
+	setTimeout(flipPage, 34000); 
+	setTimeout(flipPage, 36000);
+	setTimeout(flipPagesMain, 37000);
+}
+
+
+function flipPageMain(){
 	setInterval(flipPage, interval);
 }
 
@@ -11,6 +22,7 @@ function flipPage(){
 	// interval = 1000;
 	console.log('flipping');
 	$('.flipbook').turn('next');
+	page++; 
 }
 
 function pauseBook(){
